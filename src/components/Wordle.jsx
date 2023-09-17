374bfe4ed505fb9fe3e06a110bcf51514270b44b
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-09-17 14:03:43 +0200

    logic

--- src/components/Wordle.jsx
+++ src/components/Wordle.jsx
@@ -1,13 +1,21 @@
 import React, { useEffect } from 'react'
 import useWordle from '../hooks/UseWoordle'
+import Grid from './Grid';
 
 export default function Wordle({ solution }) {
-    const { currentGuess, handleKeyUp } = useWordle(solution);
+    const { turn, currentGuess, guesses, isCorrect, handleKeyup } = useWordle(solution);
     useEffect(() => {
-        window.addEventListener('keyup', handleKeyUp);
-        return () => window.removeEventListener('keyup', handleKeyUp);
-    }, [handleKeyUp])
+        window.addEventListener('keyup', handleKeyup);
+        return () => window.removeEventListener('keyup', handleKeyup);
+    }, [handleKeyup])
+    useEffect(() => {
+        console.log(guesses, turn, isCorrect)
+    }, [guesses, turn, isCorrect])
     return (
-        <div>Current guess - {currentGuess}</div>
+        <div>
+            <div>solution - {solution}</div>
+            <div>Current guess - {currentGuess}</div>
+            <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
+        </div>
     )
 }
